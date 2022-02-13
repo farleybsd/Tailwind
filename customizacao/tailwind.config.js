07b4d39744cc716ed10f0a0cb04a79a9fac647db
Farley Rufino <farley.rufino@apinteractive.com.br>
2022-02-13 17:54:13 -0300

    ädd custon cors

--- customizacao/tailwind.config.js
+++ customizacao/tailwind.config.js
@@ -1,7 +1,24 @@
+const {colors} = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    // colors:{ // Cuidado sobrescre as classes padrao e elas param de funcionar
+    //   black:'#010101',
+    //   red:'#A83246'
+    // },
+    // screens:{
+    //   'tablet' : '640px',
+    //   'cel':{'max':'400px'}
+    // },
+    extend: {
+      colors:{
+        'newblue':'#243C5A',
+        gray:{
+          ...colors.gray,
+          '900':'#999'
+        }
+      }
+    },
   },
   plugins: [],
 }
